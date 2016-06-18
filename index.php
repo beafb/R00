fc77840984284aa9d011270a7982568e9e9ecd0e
Nicolas MONNIER <nmonnier@e1r3p6.42.fr>
2016-06-19 01:41:55 +0200

    first

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+		<?php include("inc/html_head_info.html"); ?>
+	<body>
+		<?php include("inc/header.php"); ?>
+
+		<div id="container">
+			<div id="wrapper">
+			</div>
+		</div>
+
+		<?php include("inc/footer.php"); ?>
+	</body>
+</html>
