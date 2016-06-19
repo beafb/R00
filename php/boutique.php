1c079b55df44d9b244d8379f481557ded7770436
Nicolas MONNIER <nmonnier@e1r3p6.42.fr>
2016-06-19 05:57:24 +0200

    frefer

--- php/boutique.php
+++ php/boutique.php
@@ -9,6 +9,11 @@
   <body>
 <?php include("header.php"); ?>
     <p>Bienvenue sur ma belle boutique <?php echo $_SESSION["loggued_on_user"] ?>!!!</p>
+    <div class="market">
+      <img class="fruits" src="../img/fruit/Apple.png" alt="pom" title="une pomme mec"/>
+      <img class="fruits" src="../img/fruit/Kiwi.png" alt="Kiwi" title="un kiwi"/>
+      <img class="fruits" src="../img/fruit/Pear.png" alt="poir" title="La poire nom de dieu"/>
+    </div>
 <?php include("footer.php"); ?>
   </body>
 </html>
