b613985790b696af8dbeeabf8bd4e44c022d4ebc
Hamza BOUDRA <hboudra@e1r3p4.42.fr>
2016-06-19 12:50:15 +0200

    adding deleting user

--- php/create_db.php
+++ php/create_db.php
@@ -1,28 +1,39 @@
 <?php
-    session_start();
-    if ($_POST['title'] != NULL && $_POST['autor'] != NULL && $_POST['cat1'] != NULL && $_POST['cat2'] != NULL && $_POST['cat3'] != NULL && $_POST['price'] != NULL && $_POST['submit'] == 'OK')
-    {
-        if (!file_exists('./content'))
-        {
-            mkdir('./content');
-        }
-        if (file_exists('./content/list'))
-        {
-            $tab3 = file_get_contents('./content/list');
-            $list = unserialize($tab3);
-        }
-            $details['title'] = $_POST['title'];
-            $details['autor'] = $_POST['autor'];
-            $details['cat1'] = $_POST['cat1'];
-            $details['cat2'] = $_POST['cat2'];
-            $details['cat3'] = $_POST['cat3'];
-            $details['number'] = $_POST['number'];
-            $details['price'] = $_POST['price'];
-            $list[] = $details;
-            $list_serialized = serialize($list);
-            file_put_contents('./content/list', $list_serialized);
-            header('Location: admin.php');
-    }
-    else
-        header('Location: admin.php');
+	session_start();
+	if ($_SESSION["loggued_on_user"] === "admin") {
+	    if ($_POST['item'] != NULL && $_POST['cat'] != NULL && $_POST['submit'] == 'OK')
+	    {
+	        if (!file_exists('./content'))
+	        {
+	            mkdir('./content');
+	        }
+	        if (file_exists('./content/list'))
+	        {
+	            $tab3 = file_get_contents('./content/list');
+	            $list = unserialize($tab3);
+	        }
+	            $details['item'] = $_POST['item'];
+	            $details['cat'] = $_POST['cat'];
+				$details9['price'] = $_POST['price'];
+	            $list[] = $details;
+	            $list_serialized = serialize($list);
+	            file_put_contents('./content/list', $list_serialized);
+	    }
+?><!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+	</head>
+	<body>
+		<form action="create_db.php" method="post">
+			<p> Article <input type="Text" name="item" value=""> </p>
+			<p> catégorie <input type="Text" name="cat" value=""> </p>
+			<p> Prix <input type="Text" name="Price" value=""> </p>
+			<p> <input type="submit" name="submit" value="OK"> </p>
+		</form>
+	</body>
+</html>
+<?php
+}
 ?>
--- php/del_account.php
+++ php/del_account.php
@@ -1,9 +1,20 @@
 <?php
 	session_start();
 	if ($_SESSION['loggued_on_user']) {
-	if ($GET['']) {
-		# code...
-	}
-		$tab = unserialize(file_get_contents("../private/passwd"));
+		echo "zeubi";
+		if ($_GET['sup']) {
+			var_dump($_SESSION['loggued_on_user']);
+			$tab = unserialize(file_get_contents("../private/passwd"));
+			foreach ($tab as $key => $value) {
+				echo $tab[$key];
+				if ($value['login'] == $_SESSION['loggued_on_user']) {
+					echo "popo";
+					unset($tab[$key]);
+				}
+			}
+			$_SESSION['loggued_on_user'] = "";
+			file_put_contents("../private/passwd", serialize($tab));
+		}
 	}
  ?>
+ <a href="http://e1r3p4.42.fr:8080/R00/php/del_account.php?sup=1" title="supression definitive">cliquez ici pour suprimmer votre compte</a>
