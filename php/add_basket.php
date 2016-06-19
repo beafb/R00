f748552735e275c43649cf7aff06e3ea3679dfe7
Hamza BOUDRA <hboudra@e1r3p4.42.fr>
2016-06-19 10:19:06 +0200

    correction basket

--- php/add_basket.php
+++ php/add_basket.php
@@ -2,20 +2,24 @@
 	session_start();
 	if (isset($_GET['item'])) {
 		if (!isset($_SESSION['basket'])) {
-			echo "toz";
 			$basket['item'] = $_GET['item'];
 			$basket['qty'] = 1;
 			$_SESSION['basket'][] = $basket;
 			print_r($_SESSION['basket']);
+			exit ;
 		}
 		else {
 			foreach ($_SESSION['basket'] as $key => $value) {
-				var_dump($value);
-					if ($value['item'] == $_GET['item']) {
+				if ($value['item'] == $_GET['item']) {
 						$_SESSION['basket'][$key]['qty'] += 1;
-					echo $_SESSION['basket'][$key]['qty'];
+					print_r($_SESSION);
+					exit;
 					}
 			}
+			$basket['item'] = $_GET['item'];
+			$basket['gty'] = 1;
+			$_SESSION['basket'][] = $basket;
+			print_r($basket);
 		}
 	}
  ?>
